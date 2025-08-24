63824214e68787a394040972b7ecc1d3f8013029
Shain Sahagun <qsaqsahagun@tip.edu.ph>
2025-08-25 04:42:38 +0800

    Added new health facility data: Bangladesh, Bahrain, Canada, Chile, China, Cameroon, Congo

--- src/components/controls/HealthFacilities.tsx
+++ src/components/controls/HealthFacilities.tsx
@@ -70,4 +70,53 @@ export const healthFacilities: Record<string, HealthFacilityConfig> = {
     geojsonUrl:
       "https://tcohgcmobudcnlohjbuv.supabase.co/storage/v1/object/public/critical-facilities/health-facilities/hotosm_ben_health_facilities_points_geojson.geojson",
   },
+  Bangladesh: {
+    name: "Bangladesh",
+    center: [90.2934413, 24.4769288],
+    zoom: 7,
+    geojsonUrl:
+      "https://tcohgcmobudcnlohjbuv.supabase.co/storage/v1/object/public/critical-facilities/health-facilities/hotosm_bgd_health_facilities_points_geojson.geojson",
+  },
+  Bahrain: {
+    name: "Bahrain",
+    center: [50.5344606, 26.1551249],
+    zoom: 10,
+    geojsonUrl:
+      "https://tcohgcmobudcnlohjbuv.supabase.co/storage/v1/object/public/critical-facilities/health-facilities/hotosm_bhr_health_facilities_points_geojson.geojson",
+  },
+  Canada: {
+    name: "Canada",
+    center: [-107.991707, 61.0666922],
+    zoom: 5,
+    geojsonUrl:
+      "https://tcohgcmobudcnlohjbuv.supabase.co/storage/v1/object/public/critical-facilities/health-facilities/hotosm_can_health_facilities_points_geojson.geojson",
+  },
+  Chile: {
+    name: "Chile",
+    center: [-71.3187697, -31.7613365],
+    zoom: 3,
+    geojsonUrl:
+      "https://tcohgcmobudcnlohjbuv.supabase.co/storage/v1/object/public/critical-facilities/health-facilities/hotosm_chl_health_facilities_points_geojson.geojson",
+  },
+  China: {
+    name: "China",
+    center: [104.999927, 35.000074],
+    zoom: 4,
+    geojsonUrl:
+      "https://tcohgcmobudcnlohjbuv.supabase.co/storage/v1/object/public/critical-facilities/health-facilities/hotosm_chn_health_facilities_points_geojson.geojson",
+  },
+  Cameroon: {
+    name: "Cameroon",
+    center: [13.1535811, 4.6125522],
+    zoom: 6,
+    geojsonUrl:
+      "https://tcohgcmobudcnlohjbuv.supabase.co/storage/v1/object/public/critical-facilities/health-facilities/hotosm_cmr_health_facilities_points_geojson.geojson",
+  },
+  Congo: {
+    name: "Congo",
+    center: [23.8222636, -2.9814344],
+    zoom: 4,
+    geojsonUrl:
+      "https://tcohgcmobudcnlohjbuv.supabase.co/storage/v1/object/public/critical-facilities/health-facilities/hotosm_cog_health_facilities_points_geojson.geojson",
+  },
 };
